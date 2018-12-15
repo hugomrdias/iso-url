77070e5d24b4669fd821afed4e00991254f48631
Hugo Dias <hugomrdias@gmail.com>
2018-12-15 19:42:29 +0000

    fix: better support format in the browser

---
+++ src/url-browser.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const defaultBase = self.location ?
+    self.location.protocol + '//' + self.location.host :
+    '';
+const URL = self.URL;
+
+class URLWithLegacySupport extends URL {
+    constructor(url, base = defaultBase) {
+        super(url, base);
+        this.path = this.pathname + this.search;
+        this.auth =
+            this.username && this.password ?
+                this.username + ':' + this.password :
+                null;
+        this.query =
+            this.search && this.search.startsWith('?') ?
+                this.search.slice(1) :
+                null;
+    }
+
+    format() {
+        return this.toString();
+    }
+}
+
+function format(obj) {
+    if (typeof obj === 'string') {
+        const url = new URL(obj);
+
+        return url.toString();
+    }
+
+    if (!(obj instanceof URL)) {
+        const auth = obj.username ?
+            `${obj.username}:${obj.password}@` :
+            obj.auth || '';
+        const port = obj.port ? ':' + obj.port : '';
+        const protocol = obj.protocol ? obj.protocol + '//' : '';
+        const host = obj.host || '';
+        const hostname = obj.hostname || '';
+        const search = obj.search || (obj.query ? '?' + obj.query : '');
+        const hash = obj.hash || '';
+        const pathname = obj.pathname || '';
+        const path = obj.path || pathname + search;
+
+        return `${protocol}${auth}${host || hostname + port}${path}${hash}`;
+    }
+}
+
+module.exports = {
+    URLWithLegacySupport,
+    URLSearchParams: self.URLSearchParams,
+    defaultBase,
+    format
+};
